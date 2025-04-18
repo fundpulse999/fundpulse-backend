5aa145da85d2cd92f1f5191db763b4e839a13a87
fundpulse999 <fundpulse21@gmail.com>
2025-04-18 14:03:49 +0530

    Update SecurityConfig.java

--- src/main/java/com/fundpulse/app/config/SecurityConfig.java
+++ src/main/java/com/fundpulse/app/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // Allow frontend origin
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://fundpulse-frontend1.onrender.com")); // Allow frontend origin
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true); // Allow cookies/authentication
